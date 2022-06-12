54a822bf2a8c5d12f21dacfad90bc7ea3706218b
Portia Antwi <portiaantwi257@gmail.com>
2022-06-11 21:29:47 -0700

    control -flow-lab

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(lydia) {
+  if (lydia <= 400){
+    return "This one is on me!"
+
+  } else if (lydia > 2000 && lydia <= 2500){
+    return "I will gladly take your thirty bucks.";
+  } 
+  else {
+    return "No can do." }
+}
+  
+  
+
+
+
+function ternaryCheckCity(city){
+  const Putin = city === "NYC" ? "Ok, sounds good." : "No go.";
+  return Putin;
+
+
+  
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(greeting){
+  switch (greeting){
+    case "generous":
+    return "Thank you so much.";
+    break;
+    case "not as generous":
+      return "Thank you." ;
+      break;
+      default: return "Bye." 
+  }
+  
 }
